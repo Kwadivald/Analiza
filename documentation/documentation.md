832e39c8182119b211d564cf24d2031ffca309e5
Kwadivald <Kwadivald@users.noreply.github.com>
2026-06-30 11:50:11 +0200

    Updated the backupper script and provided documentation for it.

--- documentation/documentation.md
+++ documentation/documentation.md
@@ -17,3 +17,7 @@ This is a simple markdown-style documentation file. It gives a short overview of
 
 - This document is intended as a simple, high-level guide.
 - For more detailed explanations, inspect each file directly.
+
+## Scripts_different
+
+- [x] Backupper - a script to backup all repo data in a directory one level higher. To do this, just put a "backupper.py"  file in any directory (preferably "scripts_different") in repository and run it. It will make a "backup" directory one level higher, make inside a directory named "analiza + actual timestamp" and copy whole repo there.
--- scripts_different/backupper.py
+++ scripts_different/backupper.py
@@ -5,16 +5,20 @@ import shutil
 # Create a current timestamp for the backup folder name
 
 ts = datetime.datetime.now().strftime("%Y%m%d_%H-%M")
+print(f"Created current timestamp: {ts}") 
 
 # Create the backup folder path
 
-bfpath = f"../../backup/analiza_{ts}"
+bfpath = f"../backup/analiza_{ts}"
 print(f"Creating backup folder: {bfpath}")
 
 # Create the backup folder with the timestamp
-
-os.mkdir(bfpath)
+os.makedirs(bfpath, exist_ok=True)
+print(f"Backup folder created: {bfpath}")
 
 # Copy the contents of the parent directory to the backup folder
 
-shutil.copytree("../", bfpath, dirs_exist_ok=True)
+print(f"Copying contents of the parent directory to the backup folder: {bfpath}")
+shutil.copytree("./", bfpath, dirs_exist_ok=True, )
+print(f"Contents copied to backup folder: {bfpath}")
+print(os.listdir(f"{bfpath}"))
